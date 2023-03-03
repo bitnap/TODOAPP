1ec8c68917af7dbc60128ac16b0d2a4bd813b7dd
Bitna <bitnapark123@gmail.com>
2023-03-03 12:46:30 +0900

    index.html파일 띄워주기

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express();
-const app2 = express();
+// const app2 = express();
 
 app.listen(8080, function(){
     console.log('listening on 8080');
 });
 
-app2.listen(8000, function(){
-    console.log('listening on 8000');
-});
+// app2.listen(8000, function(){
+//     console.log('listening on 8000');
+// });
 
 
 app.get('/pet', function(요청, 응답){
     응답.send('펫용품 쇼핑하는 페이지');
 });
 
-app2.get('/beauty', function(req, res){
-    res.send('뷰티용품 쇼핑하는 페이지');
+app.get('/beauty', function(req, res){
+    res.send('뷰티용품 쇼핑 페이지');
 });
+
+app.get('/home', function(req, res){
+    res.send('홈페이지');
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html');
+})
+// app2.get('/beauty', function(req, res){
+//     res.send('뷰티용품 쇼핑하는 페이지');
+// });
