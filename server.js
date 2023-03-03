002ac6dfd95ac07e189a45b1b07a6db218dfb687
Bitna <bitnapark123@gmail.com>
2023-03-03 17:35:31 +0900

    ejs 이용해서 list.html 만들기 위해 셋팅

--- server.js
+++ server.js
@@ -4,14 +4,38 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
-
+var db;
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
+
 MongoClient.connect('mongodb+srv://admin:admin1234@cluster0.hwfu4h5.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
     if (에러) return console.log(에러);
+
+    // db = client.db('todoapp');
+
+    // db.collection('post').insertOne({제목 : 'John', _id : 1000, 날짜 : 20 }, function(에러, 결과){
+    //     console.log('저장완료');
+    // });
 
     app.listen(8080, function(){
         console.log('listening on 8080');
     });
+
+
+    app.post('/add', function(req, res){
+        res.send('전송완료');
+        console.log(req.body.date);
+        console.log(req.body.title);
+        console.log(req.body);
+
+        db = client.db('todoapp');
+
+        db.collection('post').insertOne({ 제목 : req.body.title, _id : 1, 날짜 : req.body.date}, function(에러, 결과){
+            if (에러) return console.log(에러);
+            console.log('저장완료');
+        });
+    });
+
 
 });
 
@@ -51,10 +75,3 @@ app.get('/write', function(req, res){
     res.sendFile(__dirname+'/write.html');
 });
 
-
-app.post('/add', function(req, res){
-    res.send('전송완료');
-    console.log(req.body.date);
-    console.log(req.body.title);
-    console.log(req.body);
-});
